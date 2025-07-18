e3152a2e28d40e580a9901e9453bbbeeaf3d665e
NoOPeEKS <arnauapps@gmail.com>
2025-07-19 00:59:36 +0200

    feat: hello world ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use crossterm::event::{self, Event};
+use ratatui::{Frame, text::Text};
+
+fn main() -> Result<()> {
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(draw)?;
+
+        if let Event::Key(_) = event::read()? {
+            break;
+        }
+    }
+    Ok(())
+}
+
+fn draw(frame: &mut Frame) {
+    let text = Text::raw("Hello, World!");
+    frame.render_widget(text, frame.area());
 }
