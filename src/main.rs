02348984623cc92f4da79ad2150bb05b1755f4bd
NoOPeEKS <arnauapps@gmail.com>
2025-07-20 15:32:09 +0200

    fix: cursor movement with hjkl only allowed on normal mode

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use std::fs::read_to_string;
 use std::sync::mpsc::{Receiver, channel};
 
-use crate::app::App;
+use crate::app::{App, Mode};
 use crate::events::{EventKind, handle_events};
 use crate::ui::ui;
 
@@ -47,76 +47,86 @@ fn run(
                     app.insert_mode();
                 }
                 EventKind::MoveCursorLeft => {
-                    app.current_pos.char = {
-                        // Never let current cursor position be less than 2, because char
-                        // positions 0 and 1 are occupied by line number bar's rendering.
-                        if app.current_pos.char == NUMBAR_SPACE {
-                            app.current_pos.char
-                        } else if app.current_pos.char
-                            > app.file_lines[app.current_pos.line as usize].1
-                        {
-                            app.file_lines[app.current_pos.line as usize].1 + 1
-                        } else {
-                            app.current_pos.char.saturating_sub(1)
+                    if app.mode == Mode::Normal {
+                        app.current_pos.char = {
+                            // Never let current cursor position be less than 2, because char
+                            // positions 0 and 1 are occupied by line number bar's rendering.
+                            if app.current_pos.char == NUMBAR_SPACE {
+                                app.current_pos.char
+                            } else if app.current_pos.char
+                                > app.file_lines[app.current_pos.line as usize].1
+                            {
+                                app.file_lines[app.current_pos.line as usize].1 + 1
+                            } else {
+                                app.current_pos.char.saturating_sub(1)
+                            }
                         }
                     }
                 }
                 EventKind::MoveCursorDown => {
-                    // If current line is bigger than length of lines vector - 1, limit
-                    // it to last line available. Must be len(vec) - 1 because lines start at
-                    // 0.
-                    app.current_pos.line = {
-                        if app.current_pos.line >= (app.file_lines.len() - 1) as u16 {
-                            (app.file_lines.len() - 1) as u16
-                        } else {
-                            app.current_pos.line.saturating_add(1)
-                        }
-                    };
+                    if app.mode == Mode::Normal {
+                        // If current line is bigger than length of lines vector - 1, limit
+                        // it to last line available. Must be len(vec) - 1 because lines start at
+                        // 0.
+                        app.current_pos.line = {
+                            if app.current_pos.line >= (app.file_lines.len() - 1) as u16 {
+                                (app.file_lines.len() - 1) as u16
+                            } else {
+                                app.current_pos.line.saturating_add(1)
+                            }
+                        };
 
-                    // Edge case where when going down, the line is empty line. Then put cursor
-                    // right after numbar.
-                    if app.file_lines[app.current_pos.line as usize].1 == 0_u16 {
-                        app.current_pos.char = NUMBAR_SPACE;
-                    }
-                    // If current char after going down would be bigger than the new line's
-                    // length, put it on max character of the line.
-                    else if app.current_pos.char > app.file_lines[app.current_pos.line as usize].1
-                    {
-                        app.current_pos.char =
-                            app.file_lines[app.current_pos.line as usize].1 + 1_u16;
+                        // Edge case where when going down, the line is empty line. Then put cursor
+                        // right after numbar.
+                        if app.file_lines[app.current_pos.line as usize].1 == 0_u16 {
+                            app.current_pos.char = NUMBAR_SPACE;
+                        }
+                        // If current char after going down would be bigger than the new line's
+                        // length, put it on max character of the line.
+                        else if app.current_pos.char
+                            > app.file_lines[app.current_pos.line as usize].1
+                        {
+                            app.current_pos.char =
+                                app.file_lines[app.current_pos.line as usize].1 + 1_u16;
+                        }
                     }
                 }
                 EventKind::MoveCursorUp => {
-                    app.current_pos.line = app.current_pos.line.saturating_sub(1);
-                    // Edge case where when going up the line is empty line. Then put cursor
-                    // after numbar.
-                    if app.file_lines[app.current_pos.line as usize].1 == 0_u16 {
-                        app.current_pos.char = NUMBAR_SPACE;
-                    }
-                    // If current char after going up would be bigger than the new line's
-                    // length, put it on max character of the line.
-                    else if app.current_pos.char > app.file_lines[app.current_pos.line as usize].1
-                    {
-                        app.current_pos.char =
-                            app.file_lines[app.current_pos.line as usize].1 + 1_u16;
+                    if app.mode == Mode::Normal {
+                        app.current_pos.line = app.current_pos.line.saturating_sub(1);
+                        // Edge case where when going up the line is empty line. Then put cursor
+                        // after numbar.
+                        if app.file_lines[app.current_pos.line as usize].1 == 0_u16 {
+                            app.current_pos.char = NUMBAR_SPACE;
+                        }
+                        // If current char after going up would be bigger than the new line's
+                        // length, put it on max character of the line.
+                        else if app.current_pos.char
+                            > app.file_lines[app.current_pos.line as usize].1
+                        {
+                            app.current_pos.char =
+                                app.file_lines[app.current_pos.line as usize].1 + 1_u16;
+                        }
                     }
                 }
                 EventKind::MoveCursorRight => {
-                    app.current_pos.char = {
-                        // If current line is a newly added line, default to first editor
-                        // character that starts at NUMBAR_SPACE
-                        if app.file_lines[app.current_pos.line as usize].1 == 0_u16 {
-                            NUMBAR_SPACE
-                        } else if app.current_pos.char
-                            > app.file_lines[app.current_pos.line as usize].1
-                        {
-                            // Don't really know why this works but this keeps the cursor at
-                            // the end of the line that's editing.
-                            app.file_lines[app.current_pos.line as usize].1 + 1
-                        } else {
-                            // If no constaints are being met, means we can freely add one
-                            // position to right.
-                            app.current_pos.char.saturating_add(1)
+                    if app.mode == Mode::Normal {
+                        app.current_pos.char = {
+                            // If current line is a newly added line, default to first editor
+                            // character that starts at NUMBAR_SPACE
+                            if app.file_lines[app.current_pos.line as usize].1 == 0_u16 {
+                                NUMBAR_SPACE
+                            } else if app.current_pos.char
+                                > app.file_lines[app.current_pos.line as usize].1
+                            {
+                                // Don't really know why this works but this keeps the cursor at
+                                // the end of the line that's editing.
+                                app.file_lines[app.current_pos.line as usize].1 + 1
+                            } else {
+                                // If no constaints are being met, means we can freely add one
+                                // position to right.
+                                app.current_pos.char.saturating_add(1)
+                            }
                         }
                     }
                 }
