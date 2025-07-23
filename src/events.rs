03cfb352c89429e72351829af3ed8777471cdecd
NoOPeEKS <arnauapps@gmail.com>
2025-07-23 23:00:11 +0200

    fix: make event loop thread blocking to avoid polling every cycle and reduce cpu usage

--- src/events.rs
+++ src/events.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use crossterm::event::{self, KeyCode, KeyModifiers};
-use std::{sync::mpsc::Sender, time::Duration};
+use std::sync::mpsc::Sender;
 
 pub enum EventKind {
     NormalMode,
@@ -14,23 +14,21 @@ pub enum EventKind {
 
 pub fn handle_events(sender: Sender<EventKind>) -> Result<()> {
     loop {
-        if event::poll(Duration::from_millis(20))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.modifiers.contains(KeyModifiers::CONTROL) {
-                    match key.code {
-                        KeyCode::Char('e') => sender.send(EventKind::InsertMode)?,
-                        KeyCode::Char('c') => sender.send(EventKind::Quit)?,
-                        KeyCode::Char('u') => sender.send(EventKind::ScrollUp)?,
-                        KeyCode::Char('d') => sender.send(EventKind::ScrollDown)?,
-                        _ => {}
-                    }
-                } else {
-                    match key.code {
-                        KeyCode::Esc => sender.send(EventKind::NormalMode)?,
-                        KeyCode::Char(ch) => sender.send(EventKind::KeyPressed(ch))?,
-                        KeyCode::Backspace => sender.send(EventKind::Backspace)?,
-                        _ => {}
-                    }
+        if let event::Event::Key(key) = event::read()? {
+            if key.modifiers.contains(KeyModifiers::CONTROL) {
+                match key.code {
+                    KeyCode::Char('e') => sender.send(EventKind::InsertMode)?,
+                    KeyCode::Char('c') => sender.send(EventKind::Quit)?,
+                    KeyCode::Char('u') => sender.send(EventKind::ScrollUp)?,
+                    KeyCode::Char('d') => sender.send(EventKind::ScrollDown)?,
+                    _ => {}
+                }
+            } else {
+                match key.code {
+                    KeyCode::Esc => sender.send(EventKind::NormalMode)?,
+                    KeyCode::Char(ch) => sender.send(EventKind::KeyPressed(ch))?,
+                    KeyCode::Backspace => sender.send(EventKind::Backspace)?,
+                    _ => {}
                 }
             }
         }
