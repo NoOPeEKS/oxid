378285a39905a1fff104178437c1ba8f268958b2
NoOPeEKS <arnauapps@gmail.com>
2025-08-23 04:22:30 +0200

    feat: initialization LSP flow and serializing/deserializing into requests and responses

--- oxid-lsp/src/lib.rs
+++ oxid-lsp/src/lib.rs
@@ -1,11 +1,58 @@
 use std::{
+    fmt::format,
     io::{BufRead, BufReader, Read, Write},
-    process::{Command, Stdio},
+    process::{ChildStdin, Command, Stdio},
+    sync::atomic::AtomicUsize,
 };
 
 use serde_json::json;
 
+use crate::types::{
+    ClientCapabilities, ClientInfo, HoverClientCapabilities, InitializeParams, Notification,
+    Request, Response, TextDocumentClientCapabilities,
+};
+
 mod types;
+
+static ID: AtomicUsize = AtomicUsize::new(1);
+
+fn next_id() -> usize {
+    ID.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+}
+
+fn lsp_send_request(stdin: &mut ChildStdin, req: &Request) -> anyhow::Result<()> {
+    let req = json!({
+        "jsonrpc": "2.0",
+        "id": req.id,
+        "method": req.method,
+        "params": req.params,
+    });
+
+    let body = serde_json::to_string(&req)?;
+    let req = format!("Content-Length: {}\r\n\r\n{}", body.len(), body);
+    stdin.write_all(req.as_bytes())?;
+    stdin.flush()?;
+
+    Ok(())
+}
+
+fn lsp_send_notification(
+    stdin: &mut ChildStdin,
+    notification: &Notification,
+) -> anyhow::Result<()> {
+    let req = json!({
+        "jsonrpc": "2.0",
+        "method": notification.method,
+        "params": notification.params,
+    });
+
+    let body = serde_json::to_string(&req)?;
+    let req = format!("Content-Length: {}\r\n\r\n{}", body.len(), body);
+    stdin.write_all(req.as_bytes())?;
+    stdin.flush()?;
+
+    Ok(())
+}
 
 fn run_lsp() -> anyhow::Result<()> {
     let mut lsp = Command::new("rust-analyzer")
@@ -15,21 +62,33 @@ fn run_lsp() -> anyhow::Result<()> {
     let mut stdin = lsp.stdin.take().unwrap();
     let stdout = lsp.stdout.take().unwrap();
 
-    let request = json!({
-        "jsonrpc": "2.0",
-        "id": 1,
-        "method": "initialize",
-        "params": {
-            "capabilities": {}
-        }
-    });
+    let initialization_params = InitializeParams {
+        process_id: None,
+        client_info: Some(ClientInfo {
+            name: "oxid".to_string(),
+            version: Some("0.1.0".to_string()),
+        }),
+        locale: None,
+        root_uri: Some("file:///home/beri/dev/oxid/oxid-lsp/src/lib.rs".into()),
+        initialization_options: None,
+        capabilities: ClientCapabilities {
+            text_document: Some(TextDocumentClientCapabilities {
+                hover: Some(HoverClientCapabilities {
+                    dynamic_registration: Some(true),
+                    content_format: Some(Vec::new()),
+                }),
+            }),
+        },
+        trace: None,
+    };
 
-    let body = request.to_string();
-    let header = format!("Content-Length: {}\r\n\r\n", body.len());
+    let request = Request {
+        id: next_id() as i64,
+        method: "initialize".to_string(),
+        params: Some(serde_json::to_value(&initialization_params)?),
+    };
 
-    stdin.write_all(header.as_bytes())?;
-    stdin.write_all(body.as_bytes())?;
-    stdin.flush()?;
+    lsp_send_request(&mut stdin, &request)?;
 
     let mut reader = BufReader::new(stdout);
 
@@ -46,9 +105,17 @@ fn run_lsp() -> anyhow::Result<()> {
 
     let mut body = vec![0; content_length];
     reader.read_exact(&mut body)?;
-    
+
     let body = String::from_utf8_lossy(&body);
-    println!("{body}");
+    let deserialized: Response = serde_json::from_str(&body)?;
+    println!("{deserialized:#?}");
+
+    let initialized_notification = Notification {
+        method: "initialized".to_string(),
+        params: None,
+    };
+
+    lsp_send_notification(&mut stdin, &initialized_notification)?;
 
     Ok(())
 }
--- oxid-lsp/src/types.rs
+++ oxid-lsp/src/types.rs
@@ -1,10 +1,128 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Request {
-    id: i64,
-    method: String,
-    params: Option<serde_json::Value>,
+    pub id: i64,
+    pub method: String,
+    pub params: Option<serde_json::Value>,
 }
 
-pub struct NotificationRequest {
-    method: String,
-    params: Option<serde_json::Value>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Notification {
+    pub method: String,
+    pub params: Option<serde_json::Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Response {
+    pub id: i64,
+    pub result: Option<serde_json::Value>,
+    pub error: Option<ResponseError>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ResponseError {
+    pub code: i64,
+    pub message: String,
+    pub data: Option<serde_json::Value>,
+}
+
+//--------------------------------------------------------
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct InitializeParams {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub process_id: Option<i64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub client_info: Option<ClientInfo>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub locale: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub root_uri: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub initialization_options: Option<serde_json::Value>,
+
+    pub capabilities: ClientCapabilities,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub trace: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ClientInfo {
+    pub name: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub version: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct ClientCapabilities {
+    // #[serde(skip_serializing_if = "Option::is_none")]
+    // pub workspace: Option<WorkspaceClientCapabilities>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text_document: Option<TextDocumentClientCapabilities>,
+    // #[serde(skip_serializing_if = "Option::is_none")]
+    // pub window: Option<WindowClientCapabilities>,
+
+    // #[serde(skip_serializing_if = "Option::is_none")]
+    // pub general: Option<GeneralClientCapabilities>,
+
+    // #[serde(skip_serializing_if = "Option::is_none")]
+    // pub experimental: Option<Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TextDocumentClientCapabilities {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub hover: Option<HoverClientCapabilities>,
+    // pub completion: Option<CompletionClientCapabilities>,
+    // pub synchronization: Option<TextDocumentSyncClientCapabilities>,
+    // pub signature_help: Option<SignatureHelpClientCapabilities>,
+    // pub declaration: Option<DeclarationClientCapabilities>,
+    // pub definition: Option<DefinitionClientCapabilities>,
+    // pub type_definition: Option<TypeDefinitionClientCapabilities>,
+    // pub implementation: Option<ImplementationClientCapabilities>,
+    // pub references: Option<ReferenceClientCapabilities>,
+    // pub document_highlight: Option<DocumentHighlightClientCapabilities>,
+    // pub document_symbol: Option<DocumentSymbolClientCapabilities>,
+    // pub code_action: Option<CodeActionClientCapabilities>,
+    // pub code_lens: Option<CodeLensClientCapabilities>,
+    // pub document_link: Option<DocumentLinkClientCapabilities>,
+    // pub color_provider: Option<DocumentColorClientCapabilities>,
+    // pub formatting: Option<DocumentFormattingClientCapabilities>,
+    // pub range_formatting: Option<DocumentRangeFormattingClientCapabilities>,
+    // pub on_type_formatting: Option<DocumentOnTypeFormattingClientCapabilities>,
+    // pub rename: Option<RenameClientCapabilities>,
+    // pub publish_diagnostics: Option<PublishDiagnosticsClientCapabilities>,
+    // pub folding_range: Option<FoldingRangeClientCapabilities>,
+    // pub selection_range: Option<SelectionRangeClientCapabilities>,
+    // pub linked_editing_range: Option<LinkedEditingRangeClientCapabilities>,
+    // pub call_hierarchy: Option<CallHierarchyClientCapabilities>,
+    // pub semantic_tokens: Option<SemanticTokensClientCapabilities>,
+    // pub moniker: Option<MonikerClientCapabilities>,
+    // pub type_hierarchy: Option<TypeHierarchyClientCapabilities>,
+    // pub inline_value: Option<InlineValueClientCapabilities>,
+    // pub inlay_hint: Option<InlayHintClientCapabilities>,
+    // pub diagnostic: Option<DiagnosticClientCapabilities>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HoverClientCapabilities {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dynamic_registration: Option<bool>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub content_format: Option<Vec<MarkupKind>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum MarkupKind {
+    PlainText,
+    Markdown,
 }
