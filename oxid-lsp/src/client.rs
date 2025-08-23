63d7a812215b5a3c1175b3a746c0090676cb57ab
NoOPeEKS <arnauapps@gmail.com>
2025-08-23 23:31:37 +0200

    feat: inbound messages to retrieve responses, notifications and errors

--- oxid-lsp/src/client.rs
+++ oxid-lsp/src/client.rs
@@ -6,7 +6,53 @@ use std::{
 
 use serde_json::json;
 
-use crate::types::{Notification, Request};
+use crate::types::{Notification, Request, Response, ResponseError};
+
+pub fn process_lsp_message(
+    body: &[u8],
+    rtx: &std::sync::mpsc::Sender<InboundMessage>,
+) -> anyhow::Result<()> {
+    let body = String::from_utf8_lossy(body);
+    let res: serde_json::Value = serde_json::from_str(&body)?;
+
+    // If it has error param, it's just a response error type.
+    if let Some(error) = res.get("error") {
+        let code = error["code"].as_i64().unwrap(); // Should never fail.
+        let message = error["message"].as_str().unwrap();
+        let data = error.get("data").cloned();
+
+        let rsp_error = ResponseError {
+            code,
+            message: message.to_string(),
+            data,
+        };
+        rtx.send(InboundMessage::Error(rsp_error))?;
+    }
+
+    // If it's got an id, its just a normal response.
+    if let Some(id) = res.get("id") {
+        let id = id.as_i64().unwrap();
+        let result = res["result"].clone();
+        let response = Response {
+            id,
+            result: Some(result),
+            error: None,
+        };
+        rtx.send(InboundMessage::Response(response))?;
+    } else {
+        // If it hasn't got an id and has method, its a notification
+        let method = res["method"].as_str().unwrap().to_string();
+        let params = res["params"].clone();
+
+        let notif = Notification {
+            method,
+            params: Some(params),
+        };
+        rtx.send(InboundMessage::Notification(notif))?
+    }
+
+    Ok(())
+}
 
 /// Take a program's Stdin and send a serialized LSP request.
 pub fn lsp_send_request(stdin: &mut BufWriter<ChildStdin>, req: &Request) -> anyhow::Result<()> {
@@ -111,13 +157,22 @@ pub fn start_lsp() -> anyhow::Result<LspClient> {
                     continue;
                 }
 
-                // TEMPORARY
-                let response_body = String::from_utf8_lossy(&response_body);
-                let response_json: serde_json::Value =
-                    serde_json::from_str(&response_body).unwrap();
-                response_tx
-                    .send(InboundMessage::Value(response_json))
-                    .unwrap();
+                // TODO: PROCESS LSP MESSAGE (CHECK IF ERROR, RESPONSE OR NOTIFICATION)
+                // basically do  this:
+                //
+                match process_lsp_message(&response_body, &response_tx.clone()) {
+                    Ok(_) => (),
+                    Err(_) => {
+                        continue;
+                    }
+                }
+                // let response_body = String::from_utf8_lossy(&response_body);
+                // let response_json: serde_json::Value =
+                //     serde_json::from_str(&response_body).unwrap();
+                //
+                // response_tx
+                //     .send(InboundMessage::Value(response_json))
+                //     .unwrap();
             } else {
                 // Invalid message, just continue to next iter
                 continue;
@@ -150,7 +205,9 @@ pub enum OutboundMessage {
 /// Messages that the client can receive from the LSP Server
 #[derive(Debug)]
 pub enum InboundMessage {
-    Value(serde_json::Value),
+    Response(Response),
+    Error(ResponseError),
+    Notification(Notification),
 }
 
 #[cfg(test)]
@@ -188,18 +245,21 @@ mod tests {
             trace: None,
         };
 
-        let request = Request {
+        let initialize_request = Request {
             id: next_id() as i64,
             method: "initialize".to_string(),
             params: Some(serde_json::to_value(&initialization_params).unwrap()),
         };
         lsp.request_tx
-            .send(OutboundMessage::Request(request))
+            .send(OutboundMessage::Request(initialize_request))
             .unwrap();
 
         let response = lsp.response_rx.recv().unwrap();
-        let InboundMessage::Value(val) = response;
-        let response_str = serde_json::to_string_pretty(&val).unwrap();
+        let response_str = match response {
+            InboundMessage::Response(resp) => serde_json::to_string_pretty(&resp).unwrap(),
+            InboundMessage::Error(err) => serde_json::to_string_pretty(&err).unwrap(),
+            InboundMessage::Notification(noti) => serde_json::to_string_pretty(&noti).unwrap(),
+        };
         println!("{response_str}");
 
         let initialized_notification = Notification {
@@ -226,8 +286,11 @@ mod tests {
             .unwrap();
 
         let response = lsp.response_rx.recv().unwrap();
-        let InboundMessage::Value(val) = response;
-        let response_str = serde_json::to_string_pretty(&val).unwrap();
+        let response_str = match response {
+            InboundMessage::Response(resp) => serde_json::to_string_pretty(&resp).unwrap(),
+            InboundMessage::Error(err) => serde_json::to_string_pretty(&err).unwrap(),
+            InboundMessage::Notification(noti) => serde_json::to_string_pretty(&noti).unwrap(),
+        };
         println!("{response_str}");
     }
 }
